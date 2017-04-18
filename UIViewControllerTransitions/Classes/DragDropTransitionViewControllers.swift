be45e3c70f98524d5ff285c7301f550274ce5ed8
Steve Kim <pisces@retrica.co>
2017-04-18 13:40:09 +0900

    Delete unnecessary classes

--- UIViewControllerTransitions/Classes/DragDropTransitionViewControllers.swift
+++
@@ -1,9 +0,0 @@
-//
-//  DragDropTransitionViewControllers.swift
-//  UIViewControllerTransitionsExample
-//
-//  Created by pisces on 14/04/2017.
-//  Copyright © 2017 Steve Kim. All rights reserved.
-//
-
-import Foundation
