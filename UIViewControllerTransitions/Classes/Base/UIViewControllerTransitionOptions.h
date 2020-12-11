95ec2e54e5ecd20964b75853cc13112994a920eb
steve <pisces@linecorp.com>
2020-12-11 16:24:02 +0900

    Fix to working with carthage

--- UIViewControllerTransitions/Classes/Base/UIViewControllerTransitionOptions.h
+++ UIViewControllerTransitions/Classes/Base/UIViewControllerTransitionOptions.h
@@ -31,6 +31,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface UIViewControllerTransitionOptions : NSObject
 @property (nonatomic, getter=isUsingSpring) BOOL usingSpring;
